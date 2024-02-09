34af5e6a39698ff6c56785d7cad063b4d2ebf3b0
PrimalDelta <bro20073@byui.edu>
2024-02-08 19:21:27 -0700

    Hello Team

--- NodeJSExample/helloteam.js
+++ NodeJSExample/helloteam.js
@@ -1,4 +1,4 @@
-const teamNames = ["Jacob McEwan", "Dallen Harmon", "Taylor Cazier", "DH2"];
+const teamNames = ["Jacob McEwan", "Dallen Harmon", "Taylor Cazier", "DH2", "Jared Browner-Botson"];
 
 function sayHello() {
     teamNames.forEach(name => {
